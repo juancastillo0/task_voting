18e2af79dc7a6f270332ab854195aa50484b05d0
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2022-07-13 04:43:39 +0000

    one vs one voting

---
+++ lib/src/sample_feature/one_vs_one_voting.dart
@@ -0,0 +1,56 @@
+import 'package:task_voting/src/fields/prelude.dart';
+
+import 'package:task_voting/src/sample_feature/choices_view.dart';
+import 'package:task_voting/src/sample_feature/sample_item.dart';
+
+class OneVsOneSelect extends StatelessObserverWidget {
+  const OneVsOneSelect({
+    Key? key,
+    required this.choice1,
+    required this.choice2,
+    required this.onSelect,
+  }) : super(key: key);
+
+  final SampleItemEditable choice1;
+  final SampleItemEditable choice2;
+  final void Function(SampleItemEditable) onSelect;
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    final loc = AppLocalizations.of(context)!;
+
+    return Column(
+      children: [
+        Text(
+          loc.oneVsOneSelectOne,
+          style: textTheme.headline5,
+        ),
+        Row(
+          children: [
+            Card(
+              child: InkWell(
+                onTap: () {
+                  onSelect(choice1);
+                },
+                child: ChoiceInfo(
+                  choice: choice1,
+                ),
+              ),
+            ),
+            Card(
+              child: InkWell(
+                onTap: () {
+                  onSelect(choice2);
+                },
+                child: ChoiceInfo(
+                  choice: choice2,
+                ),
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
