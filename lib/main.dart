9e5744f68c421a735a5eadbd942c6897f8f58658
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2022-07-13 06:46:24 +0000

    set up hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:file_system_access/file_system_access.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:mobx/mobx.dart';
 
 import 'src/app.dart';
@@ -19,11 +22,20 @@ void main() async {
   };
   // Set up the SettingsController, which will glue user settings to multiple
   // Flutter Widgets.
-  final settingsController = SettingsController(SettingsService());
+  await Hive.initFlutter();
+  final settingsService = SettingsService();
+  await settingsService.setUp();
+
+  final settingsController = SettingsController(settingsService);
 
   // Load the user's preferred theme while the splash screen is displayed.
   // This prevents a sudden theme change when the app is first displayed.
   await settingsController.loadSettings();
+  if (kIsWeb) {
+    try {
+      await FileSystem.instance.getPersistance();
+    } catch (_) {}
+  }
 
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
--- lib/src/settings/settings_service.dart
+++ lib/src/settings/settings_service.dart
@@ -1,4 +1,7 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+
+import 'package:hive/hive.dart';
+import 'package:task_voting/src/fields/prelude.dart';
 
 /// A service that stores and retrieves user settings.
 ///
@@ -6,12 +9,77 @@ import 'package:flutter/material.dart';
 /// persist the user settings locally, use the shared_preferences package. If
 /// you'd like to store settings on a web server, use the http package.
 class SettingsService {
-  /// Loads the User's preferred ThemeMode from local or remote storage.
-  Future<ThemeMode> themeMode() async => ThemeMode.system;
+  static late final LazyBox<Map> box;
 
-  /// Persists the user's preferred ThemeMode to local or remote storage.
-  Future<void> updateThemeMode(ThemeMode theme) async {
-    // Use the shared_preferences package to persist settings locally or the
-    // http package to persist settings over the network.
+  Future<void> setUp() async {
+    Hive.registerAdapter<Map>(_JsonAdapter(typeId: 1));
+    box = await Hive.openLazyBox('MainBox');
+  }
+
+  /// Loads the User's preferred ThemeMode from local or remote storage.
+  final themeMode = HiveKey<ThemeMode>(
+    'themeMode',
+    (json) => json == null ||
+            ThemeMode.values.every((e) => e.name != json['themeMode'])
+        ? ThemeMode.system
+        : ThemeMode.values.byName(json['themeMode'] as String),
+    (v) => {'themeMode': v.name},
+  );
+
+  final locale = HiveKey<Locale>(
+    'locale',
+    (json) => json == null ||
+            AppLocalizations.supportedLocales.every(
+              (l) =>
+                  l.languageCode != (json['languageCode'] as String) ||
+                  l.countryCode != (json['countryCode'] as String?),
+            )
+        ? const Locale('en', null)
+        : Locale(
+            json['languageCode'] as String,
+            json['countryCode'] as String?,
+          ),
+    (v) => {'languageCode': v.languageCode, 'countryCode': v.countryCode},
+  );
+}
+
+class _JsonAdapter extends TypeAdapter<Map<String, Object?>> {
+  _JsonAdapter({required this.typeId});
+
+  @override
+  final int typeId;
+
+  @override
+  Map<String, Object?> read(BinaryReader reader) {
+    final jsonString = reader.readString();
+    final json = jsonDecode(jsonString);
+    return (json as Map).cast();
+  }
+
+  @override
+  void write(BinaryWriter writer, Map<String, Object?> json) {
+    final jsonString = jsonEncode(json);
+    writer.writeString(jsonString);
+  }
+}
+
+class HiveKey<T> {
+  final String key;
+  final T Function(Map<String, Object?>?) fromJson;
+  final Map<String, Object?> Function(T) toJson;
+
+  HiveKey(this.key, this.fromJson, this.toJson);
+
+  Future<T> call() => get();
+
+  Future<T> get() async {
+    final value = await SettingsService.box.get(key);
+    return fromJson(value?.cast());
+  }
+
+  Future<Map<String, Object?>> set(T value) async {
+    final json = toJson(value);
+    await SettingsService.box.put(key, json);
+    return json;
   }
 }
