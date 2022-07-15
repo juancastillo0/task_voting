e4249efbc85524b498ed6ac375d2bd0e790c5cd7
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2022-07-15 03:53:07 +0000

    DataCell sumValue() for ice voting, scroll in ice table, choice info dialog and localization

--- lib/src/sample_feature/ice_voting.dart
+++ lib/src/sample_feature/ice_voting.dart
@@ -1,5 +1,5 @@
 import 'package:task_voting/src/fields/prelude.dart';
-
+import 'package:task_voting/src/sample_feature/choices_view.dart';
 import 'package:task_voting/src/sample_feature/sample_item.dart';
 
 class ICEVotingTable extends StatelessObserverWidget {
@@ -10,84 +10,173 @@ class ICEVotingTable extends StatelessObserverWidget {
     required this.sortIndex,
   }) : super(key: key);
 
-  final List<ItemICEAssessment> items;
+  final List<SampleItemEditable> items;
   final int? sortIndex;
   final void Function(int index, bool ascending) sortCallback;
 
   @override
   Widget build(BuildContext context) {
-    return DataTable(
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.surface,
-      ),
-      border: TableBorder.all(
-        borderRadius: BorderRadius.circular(3),
-      ),
-      horizontalMargin: 6,
-      columnSpacing: 12,
-      headingRowHeight: 32,
-      dataRowHeight: 44,
-      sortAscending: sortIndex == null || sortIndex! > 0,
-      sortColumnIndex: sortIndex?.abs(),
-      columns: [
-        const DataColumn(
-          label: Text('Title'),
+    final store = Provider.of<ChoicesStore>(context);
+    final loc = AppLocalizations.of(context)!;
+
+    DataCell sumValue(
+      int Function(ItemICEAssessment item) reducer, {
+      bool showError = true,
+    }) {
+      return DataCell(
+        Observer(builder: (context) {
+          final value =
+              items.map((e) => e.iceAssessment).map(reducer).fold<int>(
+                    0,
+                    (previousValue, element) => previousValue + element,
+                  );
+
+          return SelectableText(
+            value.toString(),
+            style: Theme.of(context).textTheme.bodyText2?.copyWith(
+                  color: showError && value > store.iceMaxValuePerVote.value
+                      ? Colors.red[900]
+                      : null,
+                ),
+          );
+        }),
+      );
+    }
+
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 120,
+              padding: const EdgeInsets.all(6.0),
+              child: ObsTextInput(
+                obs: store.iceMaxValuePerVote,
+                parser: TextInputParser.intParser,
+                textField: const TextField(
+                  decoration: InputDecoration(label: Text('Max Sum')),
+                ),
+              ),
+            ),
+          ],
         ),
-        DataColumn(
-          numeric: true,
-          label: const Text('Impact'),
-          onSort: sortCallback,
-        ),
-        DataColumn(
-          numeric: true,
-          label: const Text('Confidence'),
-          onSort: sortCallback,
-        ),
-        DataColumn(
-          numeric: true,
-          label: const Text('Easiness'),
-          onSort: sortCallback,
-        ),
-        DataColumn(
-          numeric: true,
-          label: const Text('Urgency'),
-          onSort: sortCallback,
-        ),
-        DataColumn(
-          numeric: true,
-          label: const Text('Points'),
-          onSort: sortCallback,
+        const SizedBox(height: 16),
+        Expanded(
+          child: SingleChildScrollView(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: DataTable(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.surface,
+                  // boxShadow: kElevationToShadow[1],
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                border: TableBorder.all(
+                  borderRadius: BorderRadius.circular(4),
+                  color:
+                      Theme.of(context).colorScheme.onSurface.withOpacity(0.15),
+                ),
+                horizontalMargin: 6,
+                columnSpacing: 12,
+                headingRowHeight: 32,
+                dataRowHeight: 44,
+                sortAscending: sortIndex == null || sortIndex! > 0,
+                sortColumnIndex: sortIndex?.abs(),
+                columns: [
+                  DataColumn(
+                    label: SelectableText(loc.iceTableChoiceTitle),
+                  ),
+                  DataColumn(
+                    numeric: true,
+                    label: Text(loc.iceTableImpact),
+                    onSort: sortCallback,
+                  ),
+                  DataColumn(
+                    numeric: true,
+                    label: Text(loc.iceTableConfidence),
+                    onSort: sortCallback,
+                  ),
+                  DataColumn(
+                    numeric: true,
+                    label: Text(loc.iceTableEasiness),
+                    onSort: sortCallback,
+                  ),
+                  DataColumn(
+                    numeric: true,
+                    label: Text(loc.iceTableUrgency),
+                    onSort: sortCallback,
+                  ),
+                  DataColumn(
+                    numeric: true,
+                    label: Text(loc.iceTablePoints),
+                    onSort: sortCallback,
+                  ),
+                ],
+                rows: items.map(
+                  (item) {
+                    final e = item.iceAssessment;
+                    return DataRow(
+                      key: ValueKey(item.id),
+                      cells: [
+                        DataCell(
+                          Row(
+                            children: [
+                              Expanded(
+                                child: SelectableText(item.title.value),
+                              ),
+                              RoundedIconButton(
+                                onTap: () {
+                                  showSimpleAlertDialog(
+                                    context,
+                                    ChoiceInfo(choice: item),
+                                  );
+                                },
+                                size: 20,
+                                icon: Icons.info_outline,
+                              )
+                            ],
+                          ),
+                        ),
+                        DataCell(
+                          IntInput(obs: e.impact),
+                        ),
+                        DataCell(
+                          IntInput(obs: e.confidence),
+                        ),
+                        DataCell(
+                          IntInput(obs: e.easiness),
+                        ),
+                        DataCell(
+                          IntInput(obs: e.urgency),
+                        ),
+                        DataCell(
+                          Observer(builder: (context) {
+                            return SelectableText(e.points.value.toString());
+                          }),
+                        ),
+                      ],
+                    );
+                  },
+                ).toList()
+                  ..add(DataRow(
+                    key: const ValueKey('sumFooter'),
+                    cells: [
+                      DataCell(
+                        Text(loc.iceTableSumFooter),
+                      ),
+                      sumValue((e) => e.impact.value),
+                      sumValue((e) => e.confidence.value),
+                      sumValue((e) => e.easiness.value),
+                      sumValue((e) => e.urgency.value),
+                      sumValue((e) => e.points.value, showError: false),
+                    ],
+                  )),
+              ),
+            ),
+          ),
         ),
       ],
-      rows: items
-          .map(
-            (e) => DataRow(
-              key: ValueKey(e.info.id),
-              cells: [
-                DataCell(
-                  Text(e.info.title.value),
-                ),
-                DataCell(
-                  IntInput(obs: e.impact),
-                ),
-                DataCell(
-                  IntInput(obs: e.confidence),
-                ),
-                DataCell(
-                  IntInput(obs: e.easiness),
-                ),
-                DataCell(
-                  IntInput(obs: e.urgency),
-                ),
-                DataCell(
-                  Observer(builder: (context) {
-                    return Text(e.points.value.toString());
-                  }),
-                ),
-              ],
-            ),
-          )
-          .toList(),
     );
   }
 }
