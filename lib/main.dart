e26b069d76d269d313bcf51168fb0fe66aedb971
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2022-07-15 03:40:46 +0000

    FileSystem.instance.getPersistance, prefix task_voting_ for tables and make hive keys private in settings service

--- lib/main.dart
+++ lib/main.dart
@@ -31,11 +31,6 @@ void main() async {
   // Load the user's preferred theme while the splash screen is displayed.
   // This prevents a sudden theme change when the app is first displayed.
   await settingsController.loadSettings();
-  if (kIsWeb) {
-    try {
-      await FileSystem.instance.getPersistance();
-    } catch (_) {}
-  }
 
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
--- lib/src/settings/settings_service.dart
+++ lib/src/settings/settings_service.dart
@@ -1,7 +1,9 @@
-import 'dart:convert';
+import 'dart:convert' show jsonDecode, jsonEncode;
 
+import 'package:file_system_access/file_system_access.dart';
 import 'package:hive/hive.dart';
 import 'package:task_voting/src/fields/prelude.dart';
+import 'package:task_voting/src/sample_feature/sample_item.dart';
 
 /// A service that stores and retrieves user settings.
 ///
@@ -9,15 +11,26 @@ import 'package:task_voting/src/fields/prelude.dart';
 /// persist the user settings locally, use the shared_preferences package. If
 /// you'd like to store settings on a web server, use the http package.
 class SettingsService {
+  // TODO: make them not static maybe use RootStore
   static late final LazyBox<Map> box;
+  static late final FileSystemPersistance? webFilePersistence;
 
   Future<void> setUp() async {
     Hive.registerAdapter<Map>(_JsonAdapter(typeId: 1));
-    box = await Hive.openLazyBox('MainBox');
+    box = await Hive.openLazyBox('task_voting_MainBox');
+
+    try {
+      webFilePersistence = await FileSystem.instance.getPersistance(
+        databaseName: 'task_voting_FilesDB',
+        objectStoreName: 'task_voting_FilesObjectStore',
+      );
+    } catch (_) {
+      webFilePersistence = null;
+    }
   }
 
   /// Loads the User's preferred ThemeMode from local or remote storage.
-  final themeMode = HiveKey<ThemeMode>(
+  final themeMode = HiveKey<ThemeMode>._(
     'themeMode',
     (json) => json == null ||
             ThemeMode.values.every((e) => e.name != json['themeMode'])
@@ -26,7 +39,7 @@ class SettingsService {
     (v) => {'themeMode': v.name},
   );
 
-  final locale = HiveKey<Locale>(
+  final locale = HiveKey<Locale>._(
     'locale',
     (json) => json == null ||
             AppLocalizations.supportedLocales.every(
@@ -68,7 +81,7 @@ class HiveKey<T> {
   final T Function(Map<String, Object?>?) fromJson;
   final Map<String, Object?> Function(T) toJson;
 
-  HiveKey(this.key, this.fromJson, this.toJson);
+  HiveKey._(this.key, this.fromJson, this.toJson);
 
   Future<T> call() => get();
 
@@ -81,5 +94,62 @@ class HiveKey<T> {
     final json = toJson(value);
     await SettingsService.box.put(key, json);
     return json;
+  }
+}
+
+class HiveCollectionKey<T> {
+  final String boxName;
+  final Future<T> Function(Map<String, Object?>, {T? valueToEdit}) fromJson;
+  final Future<Map<String, Object?>> Function(T) toJson;
+
+  HiveCollectionKey._(this.boxName, this.fromJson, this.toJson);
+
+  static final choicesStoreCollection = HiveCollectionKey<ChoicesStore>._(
+    'task_voting_ChoicesStoreBox',
+    (json, {valueToEdit}) async {
+      final store = valueToEdit ?? ChoicesStore();
+      final result = await store.populateFromJson(json);
+      result.when<void>(
+        ok: (ok) => ok(),
+        err: (err) {
+          if (valueToEdit == null) {
+            store.dispose();
+          }
+          throw err;
+        },
+      );
+      return store;
+    },
+    (value) async => value.toJson(),
+  );
+
+  Future<T?> call(String key) => get(key);
+
+  Future<T?> get(String key, {T? valueToEdit}) async {
+    final box = await Hive.openLazyBox<Map>(boxName);
+    final value = await box.get(key);
+    if (value == null) return null;
+    return fromJson(value.cast(), valueToEdit: valueToEdit);
+  }
+
+  Future<Map<String, Object?>> set(String key, T value) async {
+    final box = await Hive.openLazyBox<Map>(boxName);
+    final json = await toJson(value);
+    await box.put(key, json);
+    return json;
+  }
+
+  Future<List<T>> getAll() async {
+    final box = await Hive.openLazyBox<Map>(boxName);
+    final value = await Future.wait(
+      box.keys.map(
+        (key) async {
+          final map = await box.get(key);
+          return fromJson(map!.cast());
+        },
+      ),
+    );
+
+    return value;
   }
 }
