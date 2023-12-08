4666a8fc6b99d54fe00fa1662478d652303e968f
Juan Manuel Castillo <42351046+juancastillo0@users.noreply.github.com>
2023-12-08 09:26:53 -0500

    generate schema cli --watch for build_runner

--- backend_server/bin/generate_schema.dart
+++ backend_server/bin/generate_schema.dart
@@ -2,10 +2,57 @@ import 'dart:io';
 
 import 'package:backend_server/graphql_api.schema.dart';
 
-void main() {
+void main(List<String> args) async {
+  generateSchema();
+  if (!args.contains('--watch')) {
+    return;
+  }
+  var previousChange = DateTime.now();
+  Directory(Platform.script.resolve('../lib').toFilePath())
+      .watch()
+      .listen((_) async {
+    final p = previousChange;
+    previousChange = DateTime.now();
+    await Future.delayed(const Duration(seconds: 3));
+    if (p != previousChange) return;
+
+    generateSchemaCLI();
+  });
+
+  startBuildRunner(Platform.script.resolve('..').toFilePath());
+  startBuildRunner(Platform.script.resolve('../../api_client').toFilePath());
+}
+
+void generateSchema() {
   final schema = recreateGraphQLApiSchema();
   File(Platform.script
           .resolve('../../api_client/lib/schema.graphql')
           .toFilePath())
       .writeAsStringSync(schema.schemaStr);
+  print('generated schema');
+}
+
+void generateSchemaCLI() async {
+  final p = await Process.start(
+    'fvm',
+    'dart run bin/generate_schema.dart'.split(' '),
+    workingDirectory: Platform.script.resolve('..').toFilePath(),
+    mode: ProcessStartMode.inheritStdio,
+  );
+  await p.exitCode;
+}
+
+void startBuildRunner(String workingDirectory) async {
+  final dir = workingDirectory.split(RegExp(r'[/\\]')).last;
+  while (true) {
+    final p = await Process.start(
+      'fvm',
+      'dart pub run build_runner watch --delete-conflicting-outputs'.split(' '),
+      workingDirectory: workingDirectory,
+      mode: ProcessStartMode.inheritStdio,
+    );
+    await p.exitCode;
+    print('build_runner "$dir" exited, restarting in 15 seconds');
+    await Future.delayed(const Duration(seconds: 15));
+  }
 }
